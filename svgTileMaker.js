86a2a5f8bee9b45fb97f7ae3e4c7437fceb85f04
flagstone78 <iamafluteplayer@gmail>
2018-12-13 07:21:18 +0000

    Add files via upload

--- svgTileMaker.js
+++ svgTileMaker.js
@@ -54,5 +54,26 @@ function drawStuff(desc){
 			
 		}
 	}	
+	
+	
+	svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+	
+    var svgData = svg.outerHTML;
+    var preface = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+    var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
+    var svgUrl = URL.createObjectURL(svgBlob);
+	var downloadLink = document.createElement("a");
+    downloadLink.href = svgUrl;
+    downloadLink.download = 'TileSet';
+
+    document.body.appendChild(downloadLink);
+	downloadLink.innerHTML="<button> Download Svg </button>"
+
+	
 	desc.parentElement.appendChild(svg)
 }
+
+function download(file){
+	window.location = file
+}
+	
