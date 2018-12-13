85518b0309bea1a87505274631fef5e28bfff780
flagstone78 <iamafluteplayer@gmail>
2018-12-13 06:14:15 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var tileDesc = {
+	// The key is what will appear on the tile.
+	//	 Characters are allowed by lowercase wil not appear to be vertically centered
+	// The value is how many times it will appear in the set
+	numberDesc: {
+		0: 7,
+		1: 6,
+		2: 6,
+		3: 7,
+		4: 10,
+		5: 6,
+		6: 10,
+		7: 14,
+		8: 12,
+		9: 12
+	},	
+	scale: 100, //Width and height of tiles
+	strokeWidth: .001, //Thickness of tile border
+	spaceRatio: 0.1, //Ratio of the spacing between tiles to tile width and height
+	radiusRatio: 0.2, //Ratio of the radius of tile corners to the tile width and height
+	textSizeRatio: 0.8, //Ratio of the font size to the tile height
+	parentElement: document.body //Element to add the svg to
+}
+
+drawStuff(tileDesc);
---
+++ svgTileMaker.js
@@ -0,0 +1,58 @@
+function drawStuff(desc){
+	var numberlist = []; //expand list
+	for (var key in desc.numberDesc){ 
+		while(desc.numberDesc[key] > 0){
+			numberlist.push(key)
+			desc.numberDesc[key]--
+		}
+	}
+	
+	var space = desc.spaceRatio*desc.scale //spacing between tiles
+	var radius = desc.radiusRatio*desc.scale //radius of tile corners
+	var textSize = desc.textSizeRatio*desc.scale //font size
+
+	var numXTiles = Math.floor(Math.sqrt(numberlist.length)) //number of tiles in x direction
+	var numYTiles = Math.ceil(numberlist.length/numXTiles) //number of tiles in y direction
+
+	var svgWidth = numXTiles*(desc.scale+space)+1 //total width of image
+	var svgHeight = numYTiles*(desc.scale+space)+1 //total height of image
+	
+	var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg'); 
+	svg.setAttribute('width', svgWidth)
+	svg.setAttribute('height', svgHeight)
+	svg.style.fontFamily = 'helvetica'
+	svg.setAttribute('font-weight', 900)
+	svg.setAttribute('font-size', textSize)
+	
+	for( var i = 0; i<numXTiles; i++){ //line 1
+		for(var j = 0; j<numYTiles; j++){ //letters
+			var boxx = i*(desc.scale+space)+0.5*desc.strokeWidth
+			var boxy = j*(desc.scale+space)+0.5*desc.strokeWidth
+			var rect = document.createElementNS("http://www.w3.org/2000/svg",'rect')
+			rect.setAttribute('width',desc.scale)
+			rect.setAttribute('height',desc.scale)
+			rect.setAttribute('x', boxx)
+			rect.setAttribute('y', boxy)
+			rect.setAttribute('rx',radius)
+			rect.setAttribute('ry',radius)
+			rect.setAttribute('stroke','red')
+			rect.setAttribute('stroke-width',desc.strokeWidth)
+			rect.setAttribute('fill','none')
+
+			svg.appendChild(rect);
+			
+			var number = document.createElementNS("http://www.w3.org/2000/svg",'text')
+			if(i*numXTiles+j < numberlist.length){
+				number.innerHTML = numberlist[i*numYTiles+j]
+			}
+			number.setAttribute('x',(0.5*desc.scale)+ boxx)
+			number.setAttribute('y',0.5*desc.scale+0.355*textSize+boxy)
+			number.setAttribute('text-anchor','middle')
+			number.setAttribute('stroke','none')
+			number.setAttribute('fill','black')
+			svg.appendChild(number);
+			
+		}
+	}	
+	desc.parentElement.appendChild(svg)
+}
